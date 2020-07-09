d169d44be2e4049fd46358890e7d60aa75b97a98
Carlos Lozano <carlosmlozanos@gmail.com>
2020-07-09 09:13:43 +0200

    init push

---
+++ src/main/java/cmls/gs/spring/GsContractRestServiceApplication.java
@@ -0,0 +1,13 @@
+package cmls.gs.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsContractRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsContractRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmls/gs/spring/GsContractRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cmls.gs.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsContractRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
